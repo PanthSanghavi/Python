69f6d2dea57192be5e5e0a9d9cf6d9692c99e02d
PanthSanghavi <panthsanghavi49@gmail.com>
2023-06-21 15:07:31 +0530

    restro project

---
+++ fruit list enter by user.py
@@ -0,0 +1,11 @@
+f1 = input("Enter your fruit number 1: ")
+f2 = input("Enter your fruit number 2: ")
+f3 = input("Enter your fruit number 3: ")
+f4 = input("Enter your fruit number 4: ")
+f5 = input("Enter your fruit number 5: ")
+f6 = input("Enter your fruit number 6: ")
+f7 = input("Enter your fruit number 7: ")
+
+# l = ["apple", "banana", "mango", "kiwi", "orange", "dragon", "chikko"]
+myfruitlist = [f1, f2, f3, f4, f5, f6, f7]
+print(myfruitlist)
